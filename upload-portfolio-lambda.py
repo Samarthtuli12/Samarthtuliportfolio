6f2c045a11f9fe0c12ab65bf8d7f3d6185cc05a9
Samarth Tuli <Samarthtuli@yahoo.com>
2020-07-01 20:03:22 -0500

    Use SNS to notify when portfolio is deployed

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -5,6 +5,10 @@ import zipfile
 import mimetypes
 
 def lambda_handler(event, context):
+    sns = boto3.resource('sns')
+    topic= sns.Topic('arn:aws:sns:us-east-1:272611234953:deployPortfolioTopic')
+
+    try:
     s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
     portfolio_bucket = s3.Bucket('portfolio.samarthtuli.com')
@@ -21,4 +25,9 @@ def lambda_handler(event, context):
             portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
 
     print "Job Done"
+    topic.publish(Subject="Portfolio Depl", Message= "Portfolio deployed successfully!")
+  except:
+      topic.publish(Subject="Portfolio Deploy Failed", Message="The portfolio was not deployed successfully!")
+      raise
+
     return 'hello from lambda'
